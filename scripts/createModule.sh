4a7b6b632298725946880d151d6c4719bedd46df
xstefank <xstefank122@gmail.com>
2019-01-10 12:59:46 +0100

    Add antrun plugin to create modules

---
+++ scripts/createModule.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MODULE_NAME=$1
+BASE_DIR=$2
+ARTEFACT_NAME=$3
+
+MODULE_PATH=`sed -r 's#\.#/#g' <<< ${MODULE_NAME}`
+MODULE_DIR=${BASE_DIR}/target/module/${MODULE_PATH}/main
+
+# copy resources
+cp ${BASE_DIR}/src/main/resources/module/main/module.xml ${MODULE_DIR}/module.xml
+cp ${BASE_DIR}/target/${ARTEFACT_NAME} ${MODULE_DIR}
+
+echo "Module ${MODULE_NAME} has been created in the target/module directory. You can copy it to your WildFly installation"
